7223560dacdf5129b9804b76bcefdef9864ef5fd
jorgematiasyam <jorgematiasyam@gmail.com>
2021-11-11 17:34:19 -0300

    modificado

--- src/main/java/com/crowdar/examples/steps/LogInSteps.java
+++ src/main/java/com/crowdar/examples/steps/LogInSteps.java
@@ -1,35 +1,44 @@
 package com.crowdar.examples.steps;
 
-import com.crowdar.core.PageSteps;
-import com.crowdar.core.actions.MobileActionManager;
-import com.crowdar.examples.constants.LoginConstants;
-import com.crowdar.examples.services.LoginService;
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
+import com.crowdar.examples.services.LogInClockifyService;
 import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 
-/**
- * This class handles the steps in the features files and connects with the service in case of having business logic.
- * Otherwise, if it is a simple action, like clicking a button and it has nothing related to business logic, is correct to put here.
- */
-public class LogInSteps extends PageSteps {
+public class LogInSteps {
 
-    @Given("The app is loaded correctly")
-    @Then("Login page is displayed")
-    public void isLoginPageVisible() {
-        LoginService.isViewLoaded();
+    @Given("el usuario esta en la pagina de login")
+    public void elUsuarioEstaEnLaPaginadelogin() {
+        LogInClockifyService.isViewLoaded();
     }
 
-    @When("The user goes to the Sign Up page")
-    public void goToSignUp() {
-        MobileActionManager.click(LoginConstants.SIGN_UP_BUTTON_LOCATOR);
+    @When("el usuario completa su (.*) y (.*)")
+    public void elUsuarioCompletaSuEmailYPassword(String email, String password) {
+        LogInClockifyService.doLogin(email, password);
     }
 
-    @When("The user logs in the application with: (.*), (.*)")
-    public void doLoginProcess(String email, String password) {
-        LoginService.doLogin(email, password);
+    @And("el usuario presiona el boton Login")
+    public void elUsuarioPresionaElBotonLogin() {
+        LogInClockifyService.clickButton();
     }
 
+    @Then("el usuario es redirigido a la pagina principal")
+    public void elUsuarioEsRedirigidoALaPaginaPrincipal() {LogInClockifyService.isHomePageVisible(); }
 
+    @Given("el usuario esta en la pagina principal")
+    public void elUsuarioEstaEnLaPaginaPrincipal() {LogInClockifyService.isHomePageVisible(); }
+
+
+    @When("el usuario entra al menu settings")
+    public void elUsuarioEntraAlMenuSettings() {LogInClockifyService.pantallaSettings();
+    }
+
+    @And("el usuario desactiva la opcion show notifications")
+    public void elUsuarioDesactivaLaOpcionShowNotifications() {LogInClockifyService.desactivarShowNotifications();
+    }
+
+    @Then("no se muestran las notificaciones")
+    public void noSeMuestranLasNotificaciones() {LogInClockifyService.noMostrarNotificaciones();
+    }
 }
